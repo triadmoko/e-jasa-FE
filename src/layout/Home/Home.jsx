df5c71609e32b3867f7d95203b0fbe6ee4795d78
triadmokodf <triadmoko.fatrosa@codemi.co.id>
2022-05-26 22:12:42 +0700

    navbar

---
+++ src/layout/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../../Component/Navbar/Navbar";
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        
+      </div>
+    );
+  }
+}
+
+export default Home;
